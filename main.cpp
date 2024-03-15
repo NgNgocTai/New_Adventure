196b24a9e45eb625a10fa797191651135ee5b88d
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2024-03-15 23:16:23 +0700

    setSize

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include <SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include<SDL_mixer.h>
+#include<SDL_ttf.h>
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 1200;
+const int SCREEN_HEIGHT = 600;
+const int SCREEN_BPP=32;
 
 SDL_Window* gWindow = NULL; // Cửa sổ SDL
 SDL_Renderer* gRenderer = NULL; // Renderer SDL
@@ -41,37 +44,37 @@ bool init() {
     return true;
 }
 
-// Tạo texture từ một đường dẫn hình ảnh
+// Tạo texture từ một đường dẫn hình ảnh(0 sửa hàm này)
+// hàm này chuyển 1 SDL_Surface ==> SDL_Texture để rồi sau đó render ra màn bằng câu lệnh SDL_RenderCopy()
 SDL_Texture* loadTexture(std::string path) {
     SDL_Texture* newTexture = NULL;
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str()); // load dữ liệu ảnh từ đường dẫn
     if (loadedSurface == NULL) {
         printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
     } else {
-        // Xóa phông xanh cho nhân vật
-        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 255, 0));
-
-        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);// tạo texture từ ảnh đã load được (SDL_Surface ==>SDL_Texture)
         if (newTexture == NULL) {
             printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
         }
-        SDL_FreeSurface(loadedSurface);
+        SDL_FreeSurface(loadedSurface);// giải phóng dung lượng ảnh cũ
     }
     return newTexture;
 }
 
 // Tải tất cả các tài nguyên cần thiết
+//Khi muốn thêm ảnh chỉ cần cập nhật hàm này
+//Tạo biến gAddTexture = loadTexture(link)==>check
 bool loadMedia() {
     bool success = true;
     // Tải texture cho nền
     gBackgroundTexture = loadTexture("picture/bg2.png");
     if (gBackgroundTexture == NULL) {
-        printf("Failed to load background texture!\n");
+        printf("Failed to  load background texture!\n");
         success = false;
     }
 
     // Tải texture cho đối tượng
-    gObjectTexture = loadTexture("human64x91.png");
+    gObjectTexture = loadTexture("picture/human64x91.png");
     if (gObjectTexture == NULL) {
         printf("Failed to load object texture!\n");
         success = false;
@@ -116,7 +119,9 @@ int main(int argc, char* argv[]) {
                 }
                 SDL_RenderClear(gRenderer);
                 SDL_RenderCopy(gRenderer, gBackgroundTexture, NULL, NULL); // Render texture cho nền
-                SDL_RenderCopy(gRenderer, gObjectTexture, NULL, NULL); // Render texture cho đối tượng
+
+                SDL_Rect rec_ObjectTexture = {300,420,64,96} ; //xét các thông số(x,y,w,h) cho vật
+                SDL_RenderCopy(gRenderer, gObjectTexture, NULL, &rec_ObjectTexture); // Render texture cho đối tượng + set vị trí = pointer
                 SDL_RenderPresent(gRenderer); // Hiển thị renderer
             }
         }
