5399b709cdc709d268ff6b9159b2b35288b61232
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2024-03-20 21:56:46 +0700

    update cpp

--- AmoObject.cpp
+++ AmoObject.cpp
@@ -1,1 +1,32 @@
 
+
+
+
+#include "AmoObject.h"
+
+AmoObject :: AmoObject()
+{
+    p_object_ = NULL;
+    rect_.x=0;
+    rect_.y=0;
+
+    int x_val_ =0;
+    int y_val_=0;
+    is_move_=false;
+    amo_type_ =NONE;
+}
+
+AmoObject :: ~AmoObject()
+{
+
+}
+
+void AmoObject :: HandleMove()
+{
+
+}
+
+void AmoObject:: HandleInputAction(SDL_Event events)
+{
+
+}
