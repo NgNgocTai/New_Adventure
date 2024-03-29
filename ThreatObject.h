895e88b9f0ec546dd00f894a638c4a2a2d0b2767
Nguyen Ngoc Tai <23020701@vnu.edu.vn>
2024-03-29 08:12:02 +0700

    threatObject.h

---
+++ ThreatObject.h
@@ -0,0 +1,33 @@
+
+
+#ifndef THREAT_OBJECT_H
+#define THREAT_OBJECT_H
+
+#include "Common_Function.h"
+#include "BaseObject.h"
+
+#define WIDTH_THREAT 80
+#define HEIGHT_THREAT 33
+class ThreatObject : public BaseObject
+{
+    ThreatObject();
+    ~ThreatObject();
+    //Xu ly di chuyen
+    void HandleMove(const int& x_border,const int& y_border );
+    void HandleInputAction(SDL_Event events);
+
+    //Setter va Getter cho x_val, y_val
+    void set_x_val(const int& x){x_val_=x;}
+    void set_y_val(const int &y){y_val_=y;}
+
+    int get_x_val() const{return x_val_;}
+    int get_y_val() const {return y_val_;}
+
+private:
+    int x_val_;
+    int y_val_;
+
+};
+
+
+#endif // THREAT_OBJECT_H
